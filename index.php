7c4551328bf5df7479f3151265c592d14a00428c
Backins <backinsmail@gmail.com>
2018-02-04 23:05:30 +0100

    Update MVC

--- index.php
+++ index.php
@@ -2,4 +2,14 @@
 	session_start();
     require("Core/config.php");
 
-    echo DIRNAME;
+    function autoLoader($parameter)
+    {
+        $parameter .= ".class.php";
+        if(file_exists("Core/Class/".$parameter)){
+            include("Core/Class/".$parameter);
+        }
+    }
+    spl_autoload_register("autoLoader");
+
+    $Router = new Router();
+    $Router->explodeUri();
