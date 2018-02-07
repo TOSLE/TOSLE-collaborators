633301a695e7cf4ceb13c928c52b266657586e9e
Domange <backinsmail@gmail.com>
2018-02-07 21:57:43 +0100

    SaveTemplate

--- Ressources/Templates/Default/default.tpl.php
+++ Ressources/Templates/Default/default.tpl.php
@@ -7,7 +7,22 @@
     </head>
     <body>
         <header>
+            <section>
+                <article>
+                    <p>
+                        TOSLE
+                    </p>
+                </article>
+                <article>
+                    <i class="material-icons">&#xE003;</i>
+                    <div class="profil_icon">
+                        <div class="avatar_icon">
 
+                        </div>
+                        <i class="material-icons">&#xE313;</i>
+                    </div>
+                </article>
+            </section>
         </header>
     </body>
 </html>
