b11e050e0660afe6d41ef680f9aa7fb7efdb3954
Backins <backinsmail@gmail.com>
2018-02-12 23:49:28 +0100

    Update Template Default

---
+++ Ressources/Templates/Default/default/headerMenu.php
@@ -0,0 +1,11 @@
+<div>
+
+</div>
+<div class="content-menu">
+    <ul class="ul-menuProfil">
+        <li class="li-menuAction"><a href="#">Profil</a></li>
+        <li class="separator"></li>
+        <li class="li-menuAction"><a href="#">Dashboard</a></li>
+        <li class="li-menuAction"><a href="#">Logout</a></li>
+    </ul>
+</div>
