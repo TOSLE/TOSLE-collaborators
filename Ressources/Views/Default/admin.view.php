e3c8751d180cf5aab338aa156415cc4d41616342
Backins <backinsmail@gmail.com>
2018-02-16 12:02:39 +0100

    Add Admin page

---
+++ Ressources/Views/Default/admin.view.php
@@ -0,0 +1,40 @@
+<h1><?php echo ADMIN_TITLE_PAGE;?></h1>
+<section id="admin-section" class="container">
+    <div class="row">
+        <div class="col-4">
+            <article class="article-admin-green">
+                <div>
+                    <i class="material-icons">&#xE54B;</i>
+                    <h1>Dashboard</h1>
+                </div>
+            </article>
+        </div>
+        <div class="col-4">
+            <article class="article-admin-orange">
+
+            </article>
+        </div>
+        <div class="col-4">
+            <article class="article-admin-sweetblue">
+
+            </article>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <article class="article-admin-pink">
+
+            </article>
+        </div>
+        <div class="col-4">
+            <article class="article-admin-skyblue">
+
+            </article>
+        </div>
+        <div class="col-4">
+            <article class="article-admin-salmon">
+
+            </article>
+        </div>
+    </div>
+</section>
