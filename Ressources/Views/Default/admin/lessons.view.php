1cf648a54dfd3cdf72cafce4cdbf713744aa41fb
Backins <backinsmail@gmail.com>
2018-02-20 00:08:51 +0100

    Update and add article page

---
+++ Ressources/Views/Default/admin/lessons.view.php
@@ -0,0 +1,184 @@
+<h1><a href="<?php echo DIRNAME.substr($language,0,2)."/";?>admin"><?php echo MENU_BACK_OFFICE;?></a> <span>/ <?php echo ADMIN_HOME_BLOC_TITLE_LESSONS;?></span></h1>
+
+<section id="lessons-section" class="container">
+    <div class="row">
+        <div class="col-12">
+            <article class="article-col-6">
+                <div class="title-article">
+                    <h2><?php echo ADMIN_LESSONS_TITLE_ONLINE;?></h2>
+                    <a href="#" class="target-modal icon-title-article"><i class="material-icons">&#xE145;</i></a>
+                </div>
+                <div class="main-article">
+                    <div class="tab-col-6">
+                        <div class="title-tab-col">
+                            Nom du cours
+                        </div>
+                        <div class="content-tab-col">
+                            <div>
+                                Cours 1
+                            </div>
+                            <div>
+                                Cours 2
+                            </div>
+                            <div>
+                                Cours 3
+                            </div>
+                            <div>
+                                Cours 4
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-col-2">
+                        <div class="title-tab-col">
+                            Participants
+                        </div>
+                        <div class="content-tab-col">
+                            <div>
+                                12
+                            </div>
+                            <div>
+                                5
+                            </div>
+                            <div>
+                                45
+                            </div>
+                            <div>
+                                8
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-col-2">
+                        <div class="title-tab-col">
+                            Groupes
+                        </div>
+                        <div class="content-tab-col little-tab-col">
+                            <div>
+                                1/15/21/15/21/15/21/15/2
+                            </div>
+                            <div>
+                                2
+                            </div>
+                            <div>
+                                7
+                            </div>
+                            <div>
+                                Public
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-col-3">
+                        <div class="title-tab-col">
+                            Création
+                        </div>
+                        <div class="content-tab-col">
+                            <div>
+                                01/02/2018
+                            </div>
+                            <div>
+                                24/12/2016
+                            </div>
+                            <div>
+                                02/01/2017
+                            </div>
+                            <div>
+                                17/02/2018
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </div>
+        <div class="col-12">
+            <article class="article-col-6">
+                <div class="title-article">
+                    <h2><?php echo ADMIN_LESSONS_TITLE_OFFLINE;?></h2>
+                    <a href="#" class="target-modal icon-title-article"><i class="material-icons">&#xE145;</i></a>
+                </div>
+                <div class="main-article">
+                    <div class="tab-col-6">
+                        <div class="title-tab-col">
+                            Nom du cours
+                        </div>
+                        <div class="content-tab-col">
+                            <div>
+                                Cours 1
+                            </div>
+                            <div>
+                                Cours 2
+                            </div>
+                            <div>
+                                Cours 3
+                            </div>
+                            <div>
+                                Cours 4
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-col-2">
+                        <div class="title-tab-col">
+                            Participants
+                        </div>
+                        <div class="content-tab-col">
+                            <div>
+                                12
+                            </div>
+                            <div>
+                                5
+                            </div>
+                            <div>
+                                45
+                            </div>
+                            <div>
+                                8
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-col-2">
+                        <div class="title-tab-col">
+                            Groupes
+                        </div>
+                        <div class="content-tab-col little-tab-col">
+                            <div>
+                                1/15/21/15/21/15/21/15/2
+                            </div>
+                            <div>
+                                2
+                            </div>
+                            <div>
+                                7
+                            </div>
+                            <div>
+                                Public
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-col-3">
+                        <div class="title-tab-col">
+                            Création
+                        </div>
+                        <div class="content-tab-col">
+                            <div>
+                                01/02/2018
+                            </div>
+                            <div>
+                                24/12/2016
+                            </div>
+                            <div>
+                                02/01/2017
+                            </div>
+                            <div>
+                                17/02/2018
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </div>
+</section>
+
+
+<script type="text/javascript">
+    $('body').css('background', 'linear-gradient(150deg, #F1F1F1 0%, rgba(243, 243, 243, 0.917771) 50%, rgba(243, 243, 243, 0.724556) 50%, #D8B166 50%)');
+    //$('body').css('background', 'linear-gradient(160deg, #1e5799 0%,#2989d8 50%,#207cca 51%,#7db9e8 100%)');
+</script>
---
+++ Ressources/Views/Default/blog/article.view.php
@@ -0,0 +1,62 @@
+<h2><a href="<?php echo DIRNAME.substr($language,0,2)."/";?>blog"><?php echo BLOG_TITLE_PAGE;?></a> <span>/ Titre Article</span></h2>
+
+<section id="article-view-section">
+    <div id="image-article">
+        <img src="<?php echo DIRNAME;?>Tosle/Blog/Media/Images/image-exemple-03.jpg">
+    </div>
+    <article>
+        <h1>Titre article</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquet interdum placerat. Ut cursus, mi non pretium convallis, libero tellus condimentum nisl, sit amet imperdiet nulla sapien ac odio. Suspendisse
+            sit amet ultrices ipsum. Vivamus id turpis vitae massa tempor viverra nec vel purus.</br><br>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquet interdum placerat. Ut cursus, mi non pretium convallis, libero tellus condimentum nisl, sit amet imperdiet nulla sapien ac odio. Suspendisse sit amet
+            ultrices ipsum. Vivamus id turpis vitae massa tempor viverra nec vel purus.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquet interdum placerat. Ut cursus, mi non pretium convallis, libero tellus
+            condimentum nisl, sit amet imperdiet nulla sapien ac odio. Suspendisse sit amet ultrices ipsum. Vivamus id turpis vitae massa tempor viverra nec vel purus.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+            aliquet interdum placerat. Ut cursus, mi non pretium convallis, libero tellus condimentum nisl, sit amet imperdiet nulla sapien ac odio. Suspendisse sit amet ultrices ipsum. Vivamus id turpis vitae massa tempor viverra nec vel purus.</br><br>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquet interdum placerat. Ut cursus, mi non pretium convallis, libero tellus condimentum nisl, sit amet imperdiet nulla sapien ac odio. Suspendisse sit amet
+            ultrices ipsum. Vivamus id turpis vitae massa tempor viverra nec vel purus. </p>
+    </article>
+</section>
+
+<section id="article-options-section">
+    <ul>
+        <li><a href="#"><i class="material-icons">comment</i><p>6 commentaires</p></a></li>
+        <li><a href="#"><i class="material-icons">share</i><p>8 partages</p></a></li>
+        <li><a href="#"><i class="material-icons">code</i><p>Intégrer l'article</p></a></li>
+        <li><a href="#"><i class="material-icons">create</i><p>Ecrire un commentaire</p></a></li>
+    </ul>
+</section>
+
+<section id="article-lastcom-section">
+    <h3>Derniers commentaires</h3>
+    <div class="lastcom-article">
+        <div class="com-user">
+            <div>
+                <img src="<?php echo DIRNAME;?>Tosle/Users/Images/475899654133.jpg">
+            </div>
+            <div>
+                <p class="title-com">Julien Domange</p>
+                <p class="content-com">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquet interdum placerat. Ut cursus, mi non pretium convallis, libero tellus condimentum nisl, sit amet imperdiet nulla sapien ac odio. Suspendisse
+                    sit amet ultrices ipsum. Vivamus id turpis vitae massa tempor viverra nec vel purus.</br><br>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquet interdum placerat. Ut cursus, mi non pretium convallis, libero tellus condimentum nisl, sit amet imperdiet nulla sapien ac odio. Suspendisse sit amet
+                    ultrices ipsum. Vivamus id turpis vitae massa tempor viverra nec vel purus.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquet interdum placerat. Ut cursus, mi non pretium convallis, libero tellus
+                    condimentum nisl, sit amet imperdiet nulla sapien ac odio. Suspendisse sit amet ultrices ipsum. Vivamus id turpis vitae massa tempor viverra nec vel purus.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                    aliquet interdum placerat. Ut cursus, mi non pretium convallis, libero tellus condimentum nisl, sit amet imperdiet nulla sapien ac odio. Suspendisse sit amet ultrices ipsum. Vivamus id turpis vitae massa tempor viverra nec vel purus.</br><br>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas aliquet interdum placerat. Ut cursus, mi non pretium convallis, libero tellus condimentum nisl, sit amet imperdiet nulla sapien ac odio. Suspendisse sit amet
+                    ultrices ipsum. Vivamus id turpis vitae massa tempor viverra nec vel purus.
+                </p>
+                <p class="date-create">February 28 at 6:33pm</p>
+            </div>
+        </div>
+        <div class="com-user">
+            <div>
+                <img src="<?php echo DIRNAME;?>Tosle/Users/Images/475899654133.jpg">
+            </div>
+            <div>
+                <p class="title-com">Julien Domange</p>
+                <p class="content-com">Whaou ! C’est impressionnant comme article, merci pour celui-ci ! Maintenant, les maquettes !</p>
+                <p class="date-create">February 28 at 6:33pm</p>
+            </div>
+        </div>
+    </div>
+</section>
