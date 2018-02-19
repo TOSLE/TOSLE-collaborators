27ff9d79647d25f3dfb2b7c6f17d386e591c8146
Domange <backinsmail@gmail.com>
2018-02-19 11:17:41 +0100

    update chat

--- Ressources/Views/Default/chat.view.php
+++ Ressources/Views/Default/chat.view.php
@@ -16,6 +16,58 @@
             <i class="material-icons">&#xE152;</i><p>Newest</p>
         </div>
         <div class="messages-middle">
+            <a href="#" class="content-message active">
+                <div class="header-message-info">
+                    <div class="icon-message"><p>J</p></div>
+                    <div class="owner-message"><p>Julien OSPAF</p></div>
+                </div>
+                <div class="main-message-info">
+                    <button class="option-message"><i class="material-icons">&#xE5D3;</i></button>
+                    <p>Bonjour Mehdi, peux tu me dire où sont ... </p>
+                </div>
+                <div class="footer-message-info">
+                    <p>5 min.</p>
+                </div>
+            </a>
+            <a href="#" class="content-message">
+                <div class="header-message-info">
+                    <div class="icon-message"><p>M</p></div>
+                    <div class="owner-message"><p>Mehdi Maquette</p></div>
+                </div>
+                <div class="main-message-info">
+                    <button class="option-message"><i class="material-icons">&#xE5D3;</i></button>
+                    <p>Bonjour Mehdi, peux tu me dire où sont ... </p>
+                </div>
+                <div class="footer-message-info">
+                    <p>5 min.</p>
+                </div>
+            </a>
+            <a href="#" class="content-message">
+                <div class="header-message-info">
+                    <div class="icon-message"><p>N</p></div>
+                    <div class="owner-message"><p>Najla EnFait</p></div>
+                </div>
+                <div class="main-message-info">
+                    <button class="option-message"><i class="material-icons">&#xE5D3;</i></button>
+                    <p>Bonjour Mehdi, peux tu me dire où sont ... </p>
+                </div>
+                <div class="footer-message-info">
+                    <p>5 min.</p>
+                </div>
+            </a>
+            <a href="#" class="content-message">
+                <div class="header-message-info">
+                    <div class="icon-message"><p>N</p></div>
+                    <div class="owner-message"><p>Samy PHP</p></div>
+                </div>
+                <div class="main-message-info">
+                    <button class="option-message"><i class="material-icons">&#xE5D3;</i></button>
+                    <p>Bonjour Mehdi, peux tu me dire où sont ... </p>
+                </div>
+                <div class="footer-message-info">
+                    <p>5 min.</p>
+                </div>
+            </a>
             <a href="#" class="content-message">
                 <div class="header-message-info">
                     <div class="icon-message"><p>J</p></div>
@@ -69,7 +121,11 @@
                 </div>
             </a>
         </div>
-        <i class="material-icons" id="plus">add_circle</i>
+        <div href="#" class="circle-new-message">
+            <a href="#">
+                <i class="material-icons" >add_circle</i>
+            </a>
+        </div>
     </section>
     <section class="right-column">
         d
