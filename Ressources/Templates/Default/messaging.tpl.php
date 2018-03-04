5151559b256da6211a60856b4f05b020d49fd725
Backins <backinsmail@gmail.com>
2018-03-04 22:38:39 +0100

    Create messaging tpl

--- Ressources/Templates/Default/messaging.tpl.php
+++ Ressources/Templates/Default/messaging.tpl.php
@@ -1,7 +1,49 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: julien
- * Date: 04/03/2018
- * Time: 21:28
- */
+<html>
+    <head>
+        <title>TOSLE - <?php echo $PageName;?></title>
+        <meta charset="utf-8">
+        <link href="<?php echo DIRNAME;?>Public/Libraries/Framework/ospaf/css/ospaf.css" rel="stylesheet">
+        <link href="<?php echo DIRNAME;?>Public/Styles/Default/css/template_messaging.css" rel="stylesheet">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    </head>
+    <body>
+        <header>
+            <section>
+                <div>
+                    <div class="left-block">
+                        <p>TOSLE</p>
+                    </div>
+                    <div class="right-block">
+                        <i class="material-icons">&#xE003;</i>
+                        <div class="profil-icon">
+                            <div class="avatar-profil">
+                                <img src="<?php echo DIRNAME;?>Tosle/Users/Images/475899654133.jpg">
+                            </div>
+                            <i class="material-icons">&#xE313;</i>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <nav>
+                <ul>
+                    <li class="burgermenu">
+                        <a href="#"><i class="material-icons">&#xE5D2;</i><p>Menu</p></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="material-icons">&#xE80C;</i><p>Class</p></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="material-icons">&#xE853;</i><p>Profile</p></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="material-icons">&#xE02F;</i><p>Blog</p></a>
+                    </li>
+                    <li class="active">
+                        <a href="#"><i class="material-icons">&#xE0B7;</i><p>Chat</p></a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </body>
+</html>
