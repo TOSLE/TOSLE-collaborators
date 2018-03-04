02a008bb40e128d85859b01df7810bb08859b67b
Backins <backinsmail@gmail.com>
2018-03-04 21:29:25 +0100

    Rebase master

---
+++ Core/Language/en-EN/error.lang.php
@@ -0,0 +1,4 @@
+<?php
+    define("ERROR_404_CONTROLLER_MESSAGE","Oops ! Your request is not found... Please retry or contact us with code : ERROR_404");
+    define("ERROR_01_VIEW_CALL","The TPL not exist !");
+    define("ERROR_02_VIEW_CALL","The VIEW not exist !");
---
+++ Ressources/Templates/Default/messaging.tpl.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: julien
+ * Date: 04/03/2018
+ * Time: 21:28
+ */
