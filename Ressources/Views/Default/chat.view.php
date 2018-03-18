3092f491721d1d36fdb5694b7045549a8bca2e91
Backins <backinsmail@gmail.com>
2018-03-18 18:19:44 +0100

    Prepare left column

---
+++ Ressources/Views/Default/chat.view.php
@@ -0,0 +1,46 @@
+<section class="left-column">
+    <div class="content-box-left">
+        <button class="btn-tosle btn-close-window btn-dark"><i class="material-icons">&#xE5CD;</i><p>Close menu</p></button>
+        <a href="#" class="btn-tosle new-message"><i class="material-icons">&#xE158;</i> <p>New Message</p></a>
+        <ul>
+            <li><a href="#"><i class="material-icons">&#xE0CB;</i><p>In box</p><span class="notif-active">3</span></a></li>
+            <li><a href="#"><i class="material-icons">&#xE254;</i><p>Drafts</p></a></li>
+            <li><a href="#"><i class="material-icons">&#xE152;</i><p>Filter</p></a></li>
+        </ul>
+    </div>
+    <div class="content-box-left">
+        <div class="profil-info">
+            <p class="identity">LastName FirstName</p>
+            <p class="contact"><a href="mailto:contact.tosle@gmail.com">lastname.firstname@domain.com</a></p>
+        </div>
+        <div class="profil-stats">
+            <div class="conversation">
+                <p class="number">5</p>
+                <p class="description">Conversation</p>
+            </div>
+            <div class="number-message">
+                <p class="number">150</p>
+                <p class="description">Messages</p>
+            </div>
+            <div class="number-user">
+                <p class="number">33</p>
+                <p class="description">Student</p>
+            </div>
+        </div>
+    </div>
+    <div class="footer-infos">
+        <div class="more-infos">
+            <a href="#">Mention légales</a> - <a href="#">Flux RSS</a> - <a href="#">Confidentialité</a>
+        </div>
+        <div class="global-infos">
+            <p>TOSLE <i class="material-icons">&#xE90C;</i> 2018</p>
+            <p>Teaching Open Source Learning Easy</p>
+        </div>
+    </div>
+</section>
+<section class="middle-column">
+    <p>middle</p>
+</section>
+<section class="right-column">
+    <p>right</p>
+</section>
