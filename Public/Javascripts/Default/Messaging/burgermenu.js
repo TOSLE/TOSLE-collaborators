58029f543906906d118ae5e8bc8754238f951cf0
Backins <backinsmail@gmail.com>
2018-03-19 22:31:12 +0100

    Update left-column

--- Public/Javascripts/Default/Messaging/burgermenu.js
+++ Public/Javascripts/Default/Messaging/burgermenu.js
@@ -8,11 +8,21 @@ $("#close-burgermenu").click(function(){
 
 });
 $("#open-burgermenu").click(function(){
+    if(screen.width < 420){
+        $("#left-column").css("width", "650px");
+    } else {
+        $("#left-column").css("width", "350px");
+    }
     $("#fade-background-burgermenu").css("width", "100%");
-    $("#left-column").css("width", "350px");
     setTimeout(function(){
         $("#fade-background-burgermenu").css("opacity", "1");
         $("#left-column").css("opacity", "1");
     }, 200);
 
+});
+
+$(window).resize(function(){
+    if($(window).width() > 1230){
+        $("#left-column").removeAttr("style");
+    }
 });
