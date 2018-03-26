6f94a08f24593f8adf95b88bab9fe728c4136a1a
Backins <backinsmail@gmail.com>
2018-03-26 22:04:12 +0200

    Add Dashboard Chat

---
+++ Ressources/Views/Default/Admin/chat.view.php
@@ -0,0 +1,33 @@
+<section id="right-column" class="container">
+    <div class="row">
+        <div class="col-12">
+            <div>
+                <div class="content-backoffice">
+                    <div class="header-content">
+                        <h2>Last chat message</h2>
+                        <i class="material-icons">&#xE145;</i>
+                    </div>
+                    <div class="main-content">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <td colspan="2">Sender to recipient</td>
+                                    <td>Link</td>
+                                    <td>Date</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="td-content-avatar"><img src="<?php echo DIRNAME;?>Tosle/Users/Images/475899654133.jpg"></td>
+                                    <td class="td-content-text">Najla CHELLY to Samy IMMOULA</td>
+                                    <td class="td-content-icone"><i class="material-icons">&#xE157;</i></td>
+                                    <td class="td-content-date">19/03/2018</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
