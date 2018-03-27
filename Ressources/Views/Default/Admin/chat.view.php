2d0dadd8b119e8d9a9bc1b8833892a9144c797ad
Domange <julien.domange@assemblee-nationale.fr>
2018-03-27 16:58:19 +0200

    Add footer at Default&BackOffice TPL - Update BO Messaging

--- Ressources/Views/Default/Admin/chat.view.php
+++ Ressources/Views/Default/Admin/chat.view.php
@@ -1,11 +1,11 @@
 <section id="right-column" class="container">
     <div class="row">
-        <div class="col-12">
+        <div class="col-6">
             <div>
                 <div class="content-backoffice">
                     <div class="header-content">
                         <h2>Last chat message</h2>
-                        <i class="material-icons">&#xE145;</i>
+                        <a href="#" class="desactive"><i class="material-icons">&#xE145;</i></a>
                     </div>
                     <div class="main-content">
                         <table>
@@ -20,7 +20,50 @@
                                 <tr>
                                     <td class="td-content-avatar"><img src="<?php echo DIRNAME;?>Tosle/Users/Images/475899654133.jpg"></td>
                                     <td class="td-content-text">Najla CHELLY to Samy IMMOULA</td>
-                                    <td class="td-content-icone"><i class="material-icons">&#xE157;</i></td>
+                                    <td class="td-content-icone"><a href="#"><i class="material-icons">&#xE157;</i></a></td>
+                                    <td class="td-content-date">19/03/2018</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+            <div>
+                <div class="content-backoffice">
+                    <div class="header-content">
+                        <h2>View stats</h2>
+                        <a href="#" class="desactive"><i class="material-icons">&#xE145;</i></a>
+                    </div>
+                    <div class="main-content">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <td>Data type</td>
+                                    <td>Value</td>
+                                    <td>Date</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="td-content-text">Number of message</td>
+                                    <td class="td-content-number">45</td>
+                                    <td class="td-content-date">19/03/2018</td>
+                                </tr>
+                                <tr>
+                                    <td class="td-content-text">Number of conversation</td>
+                                    <td class="td-content-number">5</td>
+                                    <td class="td-content-date">19/03/2018</td>
+                                </tr>
+                                <tr>
+                                    <td class="td-content-text">Number owner message</td>
+                                    <td class="td-content-number">20</td>
+                                    <td class="td-content-date">19/03/2018</td>
+                                </tr>
+                                <tr>
+                                    <td class="td-content-text">Number user message</td>
+                                    <td class="td-content-number">25</td>
                                     <td class="td-content-date">19/03/2018</td>
                                 </tr>
                             </tbody>
