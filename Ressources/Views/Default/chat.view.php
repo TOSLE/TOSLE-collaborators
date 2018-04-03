b9b05ea2cdf9503ee293276c3420bc3170d83906
Domange <backinsmail@gmail.com>
2018-04-03 17:20:21 +0200

    Update messaging view

--- Ressources/Views/Default/chat.view.php
+++ Ressources/Views/Default/chat.view.php
@@ -102,84 +102,86 @@
     </div>
 </section>
 <section class="right-column">
-    <div class="content-infos-message">
-        <h2>Julien Domange</h2>
-        <div class="more-infos">
-            <i class="material-icons">&#xE88E;</i>
-            <p>Group 1 - Group 3IW2</p>
+    <div class="content-right-column">
+        <div class="content-infos-message">
+            <h2>Julien Domange</h2>
+            <div class="more-infos">
+                <i class="material-icons">&#xE88E;</i>
+                <p>Group 1 - Group 3IW2</p>
+            </div>
         </div>
-    </div>
-    <div class="container-message">
-        <div class="message-owner">
-            <p>
-                Bonjour ! Je suis votre intervenant et j'aurais aimé vous poser quelques questions au sujet de votre devoir. <br>
-                Seriez vous disponible ?
-            </p>
-            <p class="date-message">
-                1 h
-            </p>
+        <div class="edit-new-message">
+            <div class="content-input">
+                <textarea type="text" placeholder="<?php echo RIGHT_COLUMN_MESSAGING_NEWMESSAGE_PLACEHOLDER;?>..."></textarea>
+            </div>
+            <div class="content-button">
+                <button class="btn btn-dark"><?php echo RIGHT_COLUMN_MESSAGING_NEWMESSAGE_BUTTON;?></button>
+            </div>
         </div>
-        <div class="message-other default-message-other">
-            <p>
-                Bonjour monsieur, je suis disponible pour répondre à vos question !
-            </p>
-            <p class="date-message">
-               41 min
-            </p>
-        </div>
-        <div class="message-owner">
-            <p>
-                Super, merci ! Voici mes questions : <br>
-                - Comment avez vous trouvé le cours "découverte du web" ?<br>
-                - Avez vous des propositions pour améliorer son contenu ?<br>
-                <br>
-                D'autres questions d'ordre plus général : <br>
-                - Comment avous vous trouvé mon site ?<br>
-                <br>
-                En vous remerciant !
-            </p>
-            <p class="date-message">
-                36 min
-            </p>
-        </div>
-        <div class="message-other default-message-other">
-            <p>
-                Alors, afin de vous répondre au mieux ! Je vais tenter d'être le plus clair et honnête possible : <br>
-                - J'ai réellement apprécié ce cours, car il explique de manière assez radicale les avantages, les inconvénients, les subtilités sont vraiments intéressante et cela me pousse à poursuivre la lecture de votre cours !<br>
-                J'espère par ailleurs qu'il y aura la suite sous peu.<br>
-                - Le contenu est assez intéressant, quoi que, je pense qu'il pourrait être judicieux de travailler un peu l'humour, l'ironie ! <br>
-                <br>
-                - Enfin, la dernière question reste assez simple ! J'ai fais une recherche approfondis sur internet pour lister les sites proposant un cours de découverte, et sur un forum j'ai trouvé le votre !<br>
-                <br>
-                J'espère vous avoir été utile.
-            </p>
-            <p class="date-message">
-               25 min
-            </p>
-        </div>
-        <div class="message-owner">
-            <p>
-                Super merci pour vos réponses, je prendrais en considération vos remarques ! N'hésitez pas à me contacter grâce à cette messagerie !
-            </p>
-            <p class="date-message">
-                15 min
-            </p>
-        </div>
-        <div class="message-other default-message-other">
-            <p>
-                Mais ça me fais plaisir ! <br>C'est noté !
-            </p>
-            <p class="date-message">
-               4 min
-            </p>
-        </div>
-    </div>
-    <div class="edit-new-message">
-        <div class="content-input">
-            <textarea type="text" placeholder="<?php echo RIGHT_COLUMN_MESSAGING_NEWMESSAGE_PLACEHOLDER;?>..."></textarea>
-        </div>
-        <div class="content-button">
-            <button class="btn btn-dark"><?php echo RIGHT_COLUMN_MESSAGING_NEWMESSAGE_BUTTON;?></button>
+        <div class="container-message">
+            <div class="message-owner">
+                <p>
+                    Bonjour ! Je suis votre intervenant et j'aurais aimé vous poser quelques questions au sujet de votre devoir. <br>
+                    Seriez vous disponible ?
+                </p>
+                <p class="date-message">
+                    1 h
+                </p>
+            </div>
+            <div class="message-other default-message-other">
+                <p>
+                    Bonjour monsieur, je suis disponible pour répondre à vos question !
+                </p>
+                <p class="date-message">
+                    41 min
+                </p>
+            </div>
+            <div class="message-owner">
+                <p>
+                    Super, merci ! Voici mes questions : <br>
+                    - Comment avez vous trouvé le cours "découverte du web" ?<br>
+                    - Avez vous des propositions pour améliorer son contenu ?<br>
+                    <br>
+                    D'autres questions d'ordre plus général : <br>
+                    - Comment avous vous trouvé mon site ?<br>
+                    <br>
+                    En vous remerciant !
+                </p>
+                <p class="date-message">
+                    36 min
+                </p>
+            </div>
+            <div class="message-other default-message-other">
+                <p>
+                    Alors, afin de vous répondre au mieux ! Je vais tenter d'être le plus clair et honnête possible : <br>
+                    - J'ai réellement apprécié ce cours, car il explique de manière assez radicale les avantages, les inconvénients, les subtilités sont vraiments intéressante et cela me pousse à poursuivre la lecture de votre cours !<br>
+                    J'espère par ailleurs qu'il y aura la suite sous peu.<br>
+                    - Le contenu est assez intéressant, quoi que, je pense qu'il pourrait être judicieux de travailler un peu l'humour, l'ironie ! <br>
+                    <br>
+                    - Enfin, la dernière question reste assez simple ! J'ai fais une recherche approfondis sur internet pour lister les sites proposant un cours de découverte, et sur un forum j'ai trouvé le votre !<br>
+                    <br>
+                    J'espère vous avoir été utile.
+                </p>
+                <p class="date-message">
+                    25 min
+                </p>
+            </div>
+            <div class="message-owner">
+                <p>
+                    Super merci pour vos réponses, je prendrais en considération vos remarques ! N'hésitez pas à me contacter grâce à cette messagerie !
+                </p>
+                <p class="date-message">
+                    15 min
+                </p>
+            </div>
+            <div class="message-other default-message-other">
+                <p>
+                    Mais ça me fais plaisir ! <br>C'est noté !
+                </p>
+                <p class="date-message">
+                    4 min
+                </p>
+            </div>
         </div>
     </div>
 </section>
