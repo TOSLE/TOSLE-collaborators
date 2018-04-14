19d1e316c443b768dd420b8929b4334cb96a5ff7
Domange <backinsmail@gmail.com>
2018-04-14 22:53:45 +0200

    create button sign in & sign up

--- Ressources/Templates/Default/default.tpl.php
+++ Ressources/Templates/Default/default.tpl.php
@@ -15,13 +15,15 @@
                 <p>TOSLE</p>
             </div>
             <div class="right-block">
-                <i class="material-icons">&#xE003;</i>
+                <a href="#" class="btn btn-white-outline tosle-btn">Se connecter</a>
+                <a href="#" class="btn btn-blue tosle-btn">S'inscrire</a>
+                <!--<i class="material-icons">&#xE003;</i>
                 <div id="profil-icon" class="profil-icon">
                     <div class="avatar-profil">
                         <img src="<?php echo DIRNAME;?>Tosle/Users/Images/475899654133.jpg">
                     </div>
                     <i id="arrow-menu" class="material-icons">&#xE313;</i>
-                </div>
+                </div>-->
             </div>
         </div>
     </section>
