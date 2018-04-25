f164a83d49ddade91a2975a7b0117d1037c38025
Domange <backinsmail@gmail.com>
2018-04-25 21:28:41 +0200

    Add blog homepage

--- Ressources/Views/Default/Dashboard/blog.view.php
+++ Ressources/Views/Default/Dashboard/blog.view.php
@@ -2,7 +2,7 @@
     <div class="row">
         <section class="title-page col-12">
             <div class="marg-container">
-                <h2><a href="<?php echo DIRNAME.substr($language,0,2)."/dashboard";?>">Dashboard</a> / Blog</h2>
+                <h2><a class="btn-sm btn-dark" href="<?php echo $this->slugs["dashboardhome"];?>">Dashboard</a> <span class="additional-message-title">/ Chat</span></h2>
             </div>
         </section>
     </div>
@@ -11,9 +11,39 @@
     <div class="row">
         <div class="col-6">
             <div>
-                <div class="content-backoffice">
+                <section class="content-backoffice">
                     <div class="header-content">
-                        <h2>View numeric stats</h2>
+                        <h4>Dernières poublications</h4>
+                        <a href="#" class="desactive"><i class="material-icons">&#xE145;</i></a>
+                    </div>
+                    <div class="main-content">
+                        <table>
+                            <thead>
+                            <tr>
+                                <td>Titre</td>
+                                <td>Action</td>
+                                <td>Date publication</td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php foreach ($lastsPublication as $key => $value): ?>
+                                <tr>
+                                    <td class="td-content-text"><?php echo $value["blog_title"];?></td>
+                                    <td class="td-content-date"><?php echo $value["blog_datecreate"];?></td>
+                                    <td class="td-content-action"><a href="#" class="btn-sm btn-tosle">View</a></td>
+                                </tr>
+                            <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </section>
+            </div>
+        </div>
+        <div class="col-6">
+            <div>
+                <section class="content-backoffice">
+                    <div class="header-content">
+                        <h4>View stats</h4>
                         <a href="#" class="desactive"><i class="material-icons">&#xE145;</i></a>
                     </div>
                     <div class="main-content">
@@ -27,72 +57,29 @@
                             </thead>
                             <tbody>
                             <tr>
-                                <td class="td-content-text">Number of post</td>
+                                <td class="td-content-text">Number of message</td>
                                 <td class="td-content-number">45</td>
                                 <td class="td-content-date">19/03/2018</td>
                             </tr>
                             <tr>
-                                <td class="td-content-text">Number comments</td>
-                                <td class="td-content-number">60</td>
+                                <td class="td-content-text">Number of conversation</td>
+                                <td class="td-content-number">5</td>
                                 <td class="td-content-date">19/03/2018</td>
                             </tr>
                             <tr>
-                                <td class="td-content-text">Number owner comments</td>
-                                <td class="td-content-number">18</td>
+                                <td class="td-content-text">Number owner message</td>
+                                <td class="td-content-number">20</td>
                                 <td class="td-content-date">19/03/2018</td>
                             </tr>
                             <tr>
-                                <td class="td-content-text">Number user comment</td>
-                                <td class="td-content-number">8</td>
-                                <td class="td-content-date">19/03/2018</td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-6">
-            <div>
-                <div class="content-backoffice">
-                    <div class="header-content">
-                        <h2>Lastest posts</h2>
-                        <a href="#" class="desactive"><i class="material-icons">&#xE145;</i></a>
-                    </div>
-                    <div class="main-content">
-                        <table>
-                            <thead>
-                            <tr>
-                                <td>Title</td>
-                                <td>Action</td>
-                                <td>Date</td>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td class="td-content-text">Un étrange procédé informatique</td>
-                                <td class="td-content-number"><button class="btn btn-dark">Edit</button> <button class="btn btn-red">Delete</button></td>
-                                <td class="td-content-date">19/03/2018</td>
-                            </tr>
-                            <tr>
-                                <td class="td-content-text">Woooo ! Arrêtez tous et venez voir ça</td>
-                                <td class="td-content-number"><button class="btn btn-dark">Edit</button> <button class="btn btn-red">Delete</button></td>
-                                <td class="td-content-date">19/03/2018</td>
-                            </tr>
-                            <tr>
-                                <td class="td-content-text">L'HTML 6 pour bientôt ?</td>
-                                <td class="td-content-number"><button class="btn btn-dark">Edit</button> <button class="btn btn-red">Delete</button></td>
-                                <td class="td-content-date">19/03/2018</td>
-                            </tr>
-                            <tr>
-                                <td class="td-content-text">PHP 7, à quand son arrivé massive ?</td>
-                                <td class="td-content-number"><button class="btn btn-dark">Edit</button> <button class="btn btn-red">Delete</button></td>
+                                <td class="td-content-text">Number user message</td>
+                                <td class="td-content-number">25</td>
                                 <td class="td-content-date">19/03/2018</td>
                             </tr>
                             </tbody>
                         </table>
                     </div>
-                </div>
+                </section>
             </div>
         </div>
     </div>
