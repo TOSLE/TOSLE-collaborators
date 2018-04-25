e132437244f1bd1e6648e524ed87986ef73418df
mehdou92 <mehdou92@hotmail.fr>
2018-04-25 18:00:10 +0200

    ajout htaccess et core pour remote

---
+++ Core/config.php
@@ -0,0 +1,12 @@
+<?php
+    define("DBUSER","root");
+    define("DBPWD","root");
+    define("DBHOST","mysql"); #le nom du container docker qui gère la bdd
+    define("DBNAME","tosle_database");
+    define("DBPORT","80");
+
+    $scriptDS = (DIRECTORY_SEPARATOR == "\\")?"/":DIRECTORY_SEPARATOR;
+    define("DS", $scriptDS);
+
+    $scriptName = (dirname($_SERVER["SCRIPT_NAME"]) == "/")?"":dirname($_SERVER["SCRIPT_NAME"]);
+    define("DIRNAME", $scriptName.DS);
