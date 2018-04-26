9617f3810fe762b3844925b9c651398d2b3a5f81
mehdou92 <mehdou92@hotmail.fr>
2018-04-26 13:57:04 +0200

    ajout du register + envoi de mail avec config gmail

--- Ressources/Templates/Default/user.tpl.php
+++ Ressources/Templates/Default/user.tpl.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>TOSLE</title>
+    <title>TOSLE - Login</title>
     <meta charset="utf-8">
     <link href="<?php echo DIRNAME; ?>Public/Libraries/Framework/ospaf/css/ospaf.css" rel="stylesheet">
     <link href="<?php echo DIRNAME; ?>Public/Styles/Default/css/template_default.css" rel="stylesheet">
@@ -9,39 +9,9 @@
     <script src="<?php echo DIRNAME; ?>Public/Libraries/jQuery/jquery-3.3.1.js"></script>
 </head>
 <body>
-<header>
-    <section>
-        <div>
-            <div class="left-block">
-                <p>TOSLE</p>
-            </div>
-            <div class="right-block">
-                <?php include "Header/right_header.tpl.php"; ?>
-            </div>
-        </div>
-    </section>
-    <nav>
-        <ul>
-            <?php include "Nav/navbar_menu.tpl.php"; ?>
-        </ul>
-    </nav>
-</header>
+
 <main>
     <?php include $this->view; ?>
 </main>
-<footer>
-    <?php include "Footer/footer_default.tpl.php"; ?>
-</footer>
-<div id="positionning-box-tosle">
-    <div class="section-size-body">
-        <div class="content-box">
-            <?php include "Header/header_menu.tpl.php"; ?>
-        </div>
-    </div>
-</div>
-<!-- INCLUDE SCRIPT -->
-<script src="<?php echo DIRNAME; ?>Public/Javascripts/Default/menuprofil.js"></script>
-<script src="<?php echo DIRNAME; ?>Public/Javascripts/Default/Messaging/burgermenu.js"></script>
-<!-- INCLUDE SCRIPT -->
 </body>
 </html>
--- Ressources/Views/Default/User/register.view.php
+++ Ressources/Views/Default/User/register.view.php
@@ -1,11 +1,6 @@
 <div class="container">
     <div class="row">
         <div class="col-12">
-            <div>
-                <section class="title-page">
-                    <h2>Inscription</h2>
-                </section>
-            </div>
         </div>
     </div>
     <div class="row">
@@ -22,6 +17,18 @@
                     </section>
                 </section>
             </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6 bg-white">
+            <section class="login-bloc">
+                <article class="register-info">
+                    <p>Déjà inscrit ?
+                        <a href="<?php echo DIRNAME . substr($language, 0, 2) . "/user/connect"; ?>">
+                            Connectez-vous ici.</a>
+                    </p>
+                </article>
+            </section>
         </div>
     </div>
 </div>
