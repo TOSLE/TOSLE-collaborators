c6ca00c4412a9a373f4146260d39f4bbfc6ddc3f
Domange <backinsmail@gmail.com>
2018-05-15 14:13:44 +0200

    Add User Repository

---
+++ Core/Repository/UserRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: backin
+ * Date: 15/05/2018
+ * Time: 11:51
+ */
+
+class UserRepository extends User
+{
+    function getUserByAuthentification()
+    {
+
+    }
+}
