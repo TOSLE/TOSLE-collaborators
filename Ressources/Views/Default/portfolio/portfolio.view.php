4d7924fdda54c392be75870b94b7ac62c0f70098
Najla3 <chelly.n100@gmail.com>
2018-05-15 15:34:00 +0200

    Mon premier commit

---
+++ Ressources/Views/Default/portfolio/portfolio.view.php
@@ -0,0 +1,29 @@
+
+
+<div class= "container">
+
+
+<center>
+        <h1>Formulaire d'Upload</h1>
+        <form action =".php" method="POST">
+            <label> Files path </label>
+            <input type="text" name="file_path"/>
+                <input type="submit" value="Send File" />
+        
+</form>
+ </center>
+  <h1>Formulaire d'Upload</h1>
+        <form action ="recup.php" method="POST">
+            <label> Files path </label>
+            <input type="text" name="file_path"/>
+
+
+               <br> Formulaire d'envoi de fichier :<br />
+                <input type="text" name="file" id="" /><br />
+                <input type="submit" value="Send File" />
+                
+        
+</form>
+ </center>
+ 
+</div>
--- Src/Controllers/PortfolioController.php
+++ Src/Controllers/PortfolioController.php
@@ -15,7 +15,9 @@ class PortfolioController
      */
     function indexAction($params)
     {
-        $View = new View("default");
+        $View = new View("default","portfolio/portfolio");
+
+
     }
 
     /**
@@ -26,5 +28,76 @@ class PortfolioController
     function editAction($params)
     {
         $View = new View("default");
+
+
     }
+    /**
+     * @Route("/en/portfolio/add")
+     * @param array $params
+     * Edit portfolio action
+     */
+    function addfileAction($params)
+    {
+
+        $View = new view("default","portfolio/portfolio");
+        $fileaddportfolio->setId('Id');
+        $fileaddportfolio->setName('Name');
+        $fileaddportfolio->setValue("Value");
+        $fileaddportfolio->save();
+
+ 
+if(isset($_POST['Id'])&& isset($_POST['Name']) && isset($_POST['Value'])) {
+ 
+ 
+   if(isset($fileaddportofolio['photo1']) && $fileaddportofolio['photo1']['error']==0) { 
+    move_uploaded_file($fileaddportofio['photo1']['Name'],
+    'tosle.fr/en/portofolio/portofolio'.basename($fileaddportfolio['photo1']['Name']));
+    echo 'L\'envoi a bien été effectué';
+ 
+ 
+ 
+    } 
+ 
+ 
+          try{ // Connexion à la BDD
+          $bdd=new PDO('mysql:host=root;dbname=tosle_database', 'tosle_file','file_path');
+ 
+          }
+ 
+          catch(Exception $e){
+          die ('Erreur:'.$e->getMessage());
+ 
+          }
+ 
+ 
+ 
+            
+        $stockage='tosle.fr/en/portofolio/portofolio'.$_['photo1']['Name'].'';
+        $insertion=$bdd->prepare('INSERT INTO () VALUES ()');       
+        $insertion->execute(array(
+ 'Id' => $_POST['Id'],
+ 'Name' => $_POST['Name'],
+ 'Value' => $_POST['Value'],
+
+));                          
+            if($insertion==true) {
+            echo '<p> Les données ont bien été enregistrées</p>';
+            }
+            else {
+            echo 'Erreur dans l\'enregistrement des données </p>';
+                } 
+ 
+ 
+ 
+ 
+ 
+            $insertion->closeCursor(); // déconnexion
+ 
+}
+?>
+
+
+
+);
+
 }
