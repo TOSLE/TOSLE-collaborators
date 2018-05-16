826e8315b581a8106851f132084cf3c30e50eea3
Domange <backinsmail@gmail.com>
2018-05-16 15:15:50 +0200

    Modification du style de la page login et ajout du captcha

--- Ressources/Modals/form.md.php
+++ Ressources/Modals/form.md.php
@@ -5,10 +5,19 @@
 <?php endif;?>
 
 <form method="<?php echo $config['config']['method'];?>" action="<?php echo $config['config']['action'];?>">
-    <?php foreach ($config["input"] as $name => $attributs):?>
-        <?php if($attributs["type"]=="text" || $attributs["type"]=="email" || $attributs["type"]=="number" || $attributs["type"]=="password"):?>
-            <input type="<?php echo $attributs["type"];?>" placeholder="<?php echo $attributs["placeholder"];?>" name="<?php echo $name;?>" <?php echo (isset($attributs["required"]))?"required='required'":"";?>><br>
+    <div class="form-group-base">
+        <?php foreach ($config["input"] as $name => $attributs):?>
+            <?php if($attributs["type"]=="text" || $attributs["type"]=="email" || $attributs["type"]=="number" || $attributs["type"]=="password"):?>
+                <input type="<?php echo $attributs["type"];?>" placeholder="<?php echo $attributs["placeholder"];?>" name="<?php echo $name;?>" <?php echo (isset($attributs["required"]))?"required='required'":"";?>><br>
+            <?php endif;?>
+        <?php endforeach;?>
+        <?php if(isset($config["captcha"])):?>
+            <label for="captcha" class="captcha img">
+                <img src="<?php echo DIRNAME;?>Public/Libraries//TosleCaptcha/GenerateCaptcha.php">
+            </label>
+            <input id="captcha" type="text" name="captcha" placeholder="Veuillez saisir le captcha présent ci-dessus">
+            <div class="small-precision-input">Vous pouvez recharger la page pour générer un nouveau captcha</div>
         <?php endif;?>
-    <?php endforeach;?>
-    <input type="submit" value="<?php echo $config["config"]["submit"];?>">
+        <input type="submit" class="btn btn-tosle" value="<?php echo $config["config"]["submit"];?>">
+    </div>
 </form>
--- Ressources/Views/Default/User/connect.view.php
+++ Ressources/Views/Default/User/connect.view.php
@@ -1,18 +1,19 @@
-<div class="container">
+<div class="container bloc-login-container">
     <div class="row">
         <div class="col-12">
         </div>
     </div>
     <div class="row">
-        <div class="col-6 bg-white">
-            <div>
+        <div class="col-3"></div>
+        <div class="col-6">
+            <div class="bg-white">
                 <section class="login-bloc">
-                    <h4>Bienvenue sur le CMS Tosle</h4>
-                    <article>
+                    <h2>Bienvenue sur le CMS Tosle</h2>
+                    <article class="register-login">
                         <p>
                             Veuillez entrer vos informations de connexion ci-dessous
                         </p></article>
-                    <section class="input-login-bloc">
+                    <section>
                         <?php $this->addModal("form", $config, $errors); ?>
                     </section>
                 </section>
@@ -20,15 +21,19 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-6 bg-white">
-            <section class="login-bloc">
-                <article class="register-info">
-                    <p>Pas encore de compte ?
-                        <a href="<?php echo DIRNAME . substr($language, 0, 2) . "/user/register"; ?>">
-                            Inscrivez-vous ici.</a>
-                    </p>
-                </article>
-            </section>
+        <div class="col-3"></div>
+        <div class="col-6">
+            <div class="bg-white">
+                <section class="login-bloc">
+                    <article class="register-info">
+                        <p>Pas encore de compte ?
+                            <a href="<?php echo DIRNAME . substr($language, 0, 2) . "/user/register"; ?>">
+                                Inscrivez-vous ici.</a>
+                        </p>
+                    </article>
+                </section>
+            </div>
+
         </div>
     </div>
 </div>
