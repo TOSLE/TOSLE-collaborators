01e3dd0d87c9876f96316816ce49ed3928a05544
mehdou92 <mehdou92@hotmail.fr>
2018-05-31 17:36:31 +0200

    reglage rebase 2

--- Kernel/Class/Authentification.php
+++ Kernel/Class/Authentification.php
@@ -1,7 +1,50 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Mehdi
- * Date: 31/05/2018
- * Time: 17:33
+ * User: jdomange
+ * Date: 11/04/2018
+ * Time: 11:35
  */
+class Authentification
+{
+    public static function checkAuthentification($token, $email)
+    {
+        $User = new User();
+        $target=['id', 'token', 'email', 'dateconnection'];
+        $User->setWhereParameter(["LIKE" => [
+            'token' => $token,
+            'email' => $email
+        ]]);
+        $User->getOneData($target);
+        if(empty($User->getToken())){
+            session_destroy();
+            return null;
+        }
+        $date = new DateTime();
+        $dateCompare = new DateTime($User->getDateconnection());
+        $interval = $date->diff($dateCompare);
+        if($interval->i > 30){
+            session_destroy();
+            return -1;
+        }
+        $User->setToken();
+        $User->setDateConnection($date->getTimestamp());
+        $User->save();
+        $_SESSION["token"]=$User->getToken();
+        $_SESSION["email"]=$User->getEmail();
+        return true;
+    }
+    public static function getUserStatus($token, $email)
+    {
+        $target=[
+            'status'
+        ];
+        $User = new User();
+        $User->setWhereParameter(["LIKE" => [
+            'token' => $token,
+            'email' => $email
+        ]]);
+        $User->getOneData($target);
+        return intval($User->getStatus());
+    }
+}
