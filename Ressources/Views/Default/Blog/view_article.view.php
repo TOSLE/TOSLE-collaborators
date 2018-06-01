1b1efb36bbd3ec7a4a3c0296842dbc091b62cce2
mehdou92 <mehdou92@hotmail.fr>
2018-06-01 12:07:39 +0200

    nettoyage projet apres rebase

--- Ressources/Views/Default/Blog/view_article.view.php
+++ Ressources/Views/Default/Blog/view_article.view.php
@@ -1,1 +1,18 @@
-<p>Coucou</p>
+<section class="main-header container">
+    <div class="row">
+        <section class="title-page col-12">
+            <div class="marg-container">
+                <h2><a class="btn-sm btn-dark" href="<?php echo $this->slugs["blog-homepage"] ?>">Blog</a><span
+                            class="additional-message-title">/ Article</span></h2>
+            </div>
+        </section>
+    </div
+</section>
+<div class="row">
+    <section class="col-10">
+        <div>
+            <div class=""></div>
+        </div>
+
+    </section>
+</div>
