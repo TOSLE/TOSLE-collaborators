6eaba3f53f8996d0eea139c50b51140e4b8365c5
Backins <backinsmail@gmail.com>
2018-06-09 22:52:39 +0200

    Sauvegarde de code

--- Kernel/globalconfig.php
+++ Kernel/globalconfig.php
@@ -1,5 +1,16 @@
 <?php
-    $scriptDS = (DIRECTORY_SEPARATOR == "\\")?"/":DIRECTORY_SEPARATOR;
+/**
+ * ENV VAR
+ */
+    $modeDev = true;
+
+    if($modeDev)
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')
+            define("SYSTEM", "WINDOWS");
+        else
+            define("SYSTEM", "LINUX");
+
+    $scriptDS = (DIRECTORY_SEPARATOR == "\\")?"\\":DIRECTORY_SEPARATOR;
     define("DS", $scriptDS);
 
     $scriptName = (dirname($_SERVER["SCRIPT_NAME"]) == "/")?"":dirname($_SERVER["SCRIPT_NAME"]);
