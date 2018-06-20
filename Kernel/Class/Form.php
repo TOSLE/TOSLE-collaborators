68657c8f516de80fa435358ecfa03ba1d2f345ff
Domange <backinsmail@gmail.com>
2018-06-20 14:09:15 +0200

    Adaption pour l'ajout des commentaires dans le blog

--- Kernel/Class/Form.php
+++ Kernel/Class/Form.php
@@ -11,32 +11,35 @@ class Form
     public static function checkForm($config, $data)
     {
         $errorsMsg = [];
-        foreach ($config["input"] as $name => $attributs) {
-            if(isset($attributs["confirm"]) && $data[$name] != $data[$attributs["confirm"]]){
-                $errorsMsg["confirmation"]= $name . 'ne correspond pas à '.$attributs["confirm"];
-            } else if (!isset($attributs["confirm"])){
-                if($attributs["type"]=="email" && !self::checkEmail($data[$name])){
-                    $errorsMsg["email"]= "Format de l'email incorrect";
-                } else if($attributs["type"]=="password" && !self::checkPwd($data[$name])){
-                    $errorsMsg["password"]= "Mot de passe incorrect(Maj, Min, Chiffre, au minimum 6 caractères)";
-                } else if($attributs["type"]=="number" && !self::checkNumber($data[$name])){
-                    $errorsMsg["number"]= $name. " n'est pas correct";
+        if(isset($config['input'])){
+            foreach ($config["input"] as $name => $attributs) {
+                if(isset($attributs["confirm"]) && $data[$name] != $data[$attributs["confirm"]]){
+                    $errorsMsg["confirmation"]= $name . 'ne correspond pas à '.$attributs["confirm"];
+                } else if (!isset($attributs["confirm"])){
+                    if($attributs["type"]=="email" && !self::checkEmail($data[$name])){
+                        $errorsMsg["email"]= "Format de l'email incorrect";
+                    } else if($attributs["type"]=="password" && !self::checkPwd($data[$name])){
+                        $errorsMsg["password"]= "Mot de passe incorrect(Maj, Min, Chiffre, au minimum 6 caractères)";
+                    } else if($attributs["type"]=="number" && !self::checkNumber($data[$name])){
+                        $errorsMsg["number"]= $name. " n'est pas correct";
+                    }
+                }
+
+                if(isset($attributs["maxString"]) && !self::maxString($data[$name], $attributs["maxString"])){
+                    $errorsMsg["longueur"]= $name. " doit faire moins de ".$attributs["maxString"];
+                }
+                if(isset($attributs["minString"]) && !self::minString($data[$name], $attributs["minString"])){
+                    $errorsMsg["longueur"]= $name. " doit faire plus de ".$attributs["minString"];
+                }
+                if(isset($attributs["maxNum"]) && !self::maxNum($data[$name], $attributs["maxNum"])){
+                    $errorsMsg["longueur"]= $name. " doit faire moins de ".$attributs["maxNum"];
+                }
+                if(isset($attributs["minNum"]) && !self::minString($data[$name], $attributs["minNum"])){
+                    $errorsMsg["longueur"]= $name. " doit faire plus de ".$attributs["minNum"];
                 }
             }
-
-            if(isset($attributs["maxString"]) && !self::maxString($data[$name], $attributs["maxString"])){
-                $errorsMsg["longueur"]= $name. " doit faire moins de ".$attributs["maxString"];
-            }
-            if(isset($attributs["minString"]) && !self::minString($data[$name], $attributs["minString"])){
-                $errorsMsg["longueur"]= $name. " doit faire plus de ".$attributs["minString"];
-            }
-            if(isset($attributs["maxNum"]) && !self::maxNum($data[$name], $attributs["maxNum"])){
-                $errorsMsg["longueur"]= $name. " doit faire moins de ".$attributs["maxNum"];
-            }
-            if(isset($attributs["minNum"]) && !self::minString($data[$name], $attributs["minNum"])){
-                $errorsMsg["longueur"]= $name. " doit faire plus de ".$attributs["minNum"];
-            }
         }
+
         if(isset($config["captcha"])){
             if(isset($_SESSION["captcha"]) && isset($data['captcha'])) {
                 if($_SESSION["captcha"] != $data['captcha']){
