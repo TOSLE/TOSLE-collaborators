937fc1a824d99960950418a6c7feb7cf660bc552
Domange <backinsmail@gmail.com>
2018-06-21 13:02:51 +0200

    Ajout ok, faut juste corriger un probleme de droit

--- Src/Controllers/ChapterController.php
+++ Src/Controllers/ChapterController.php
@@ -15,14 +15,22 @@ class ChapterController
          * On regarde si nous avons bien un paramètre dans une URL
          */
         if(isset($params["URI"][0])){
-            $getTypeURI = $params["URI"][0];
-            $View = new View("dashboard", "Dashboard/add_chapter");
+            $getTypeNewArticle = $params["URI"][0];
             $Chapter = new ChapterRepository();
+            $View = new View("dashboard", "Dashboard/add_chapter");
             $View->setData("errors", "");
+            if((isset($_FILES) && !empty($_FILES)) || (isset($params["POST"]) && !empty($params["POST"]))){
+                $resultAdd = $Chapter->addChapter($_FILES, $params["POST"]);
+                if($resultAdd === 1){
+                    header('Location:'.$routes['dashboard_lesson']);
+                } else {
+                    $View->setData("errors", $resultAdd);
+                }
+            }
 
-            if($getTypeURI == "chapter"){
+            if($getTypeNewArticle == "chapter"){
                 $View->setData("configForm", $Chapter->configFormAdd());
-            } else {
+            }  else {
                 header('Location:'.$routes['dashboard_lesson'].'/error');
             }
         } else {
