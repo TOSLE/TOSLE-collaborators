5c98e8cbc8bfe2c2426f078a6a7db63765a52f3b
Backins <backinsmail@gmail.com>
2018-06-23 01:37:01 +0200

    Correctif nom de chemin dans les add lesson/chapter

--- Ressources/Views/Default/Dashboard/add_lesson.view.php
+++ Ressources/Views/Default/Dashboard/add_lesson.view.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="title-page col-12">
             <div class="marg-container">
-                <h2><a class="btn-sm btn-dark" href="<?php echo $this->slugs["dashboardhome"];?>">Dashboard</a> <span class="additional-message-title">/ <a class="btn-sm btn-dark" href="<?php echo $this->slugs["dashboard_lesson"];?>">Lesson</a></span> <span class="additional-message-title">/ Add</span></h2>
+                <h2><a class="btn-sm btn-dark" href="<?php echo $this->slugs["dashboardhome"];?>">Dashboard</a> <span class="additional-message-title">/ <a class="btn-sm btn-dark" href="<?php echo $this->slugs["dashboard_lesson"];?>">Lesson</a></span> <span class="additional-message-title">/ Add lesson</span></h2>
             </div>
         </div>
     </div>
