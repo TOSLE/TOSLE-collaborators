6d09dcd186cfa8f033cafcce26ff7b748c5bec2d
mehdou92 <mehdou92@hotmail.fr>
2018-06-28 00:53:57 +0200

    maj profile

--- Ressources/Views/Default/Profile/profile.view.php
+++ Ressources/Views/Default/Profile/profile.view.php
@@ -11,13 +11,31 @@
     <div class="row">
         <div class="col-4">
             <div>
-                <section class="profil">
-                    <section class="picture">
-                        <div class="profil-picture">
-                            <img src="<?php echo DIRNAME;?>Tosle/Users/Images/475899654133.jpg">
-                        </div>
-                    </section>
-                    <section class="info">info</section>
+                <section class="profile">
+                    <div class="profile-container">
+                        <section class="picture">
+                            <div class="profil-picture">
+                                <img src="<?php echo DIRNAME; ?>Tosle/Users/Images/475899654133.jpg">
+                            </div>
+                        </section>
+                        <section class="info">
+                            <section class="name">
+                                <span>Julien Domange</span>
+                            </section>
+                            <section class="email">
+                                <span>Contact mail :</span>
+                                <span>julienbg92@gmail.com</span>
+                            </section>
+                            <section class="group">
+                                <span>Group:</span>
+                                <span>3IW2</span>
+                            </section>
+                            <section class="newsletter">
+                                <span>Newsletter :</span>
+                                <span>You're suscribing to the Newsletter.</span>
+                            </section>
+                        </section>
+                    </div>
                 </section>
             </div>
         </div>
