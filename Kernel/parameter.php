93bc589a268c35700e0b0d8d43d61d09506f6902
TOSLE <35349648+TOSLE@users.noreply.github.com>
2018-07-08 16:12:34 +0200

    Delete parameter.php

--- Kernel/parameter.php
+++
@@ -1,9 +0,0 @@
-<?php
-    define("DBUSER","root");
-    define("DBPWD","");
-    define("DBHOST","localhost");
-    define("DBNAME","tosle_database");
-    define("DBPORT","3306");
-    define("GUSER", "contact.tosle@gmail.com");
-    define("GPWD", "ostosle2018");
-
