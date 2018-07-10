223554f77ddd1e989f11fb6f99ddb9f96c79e441
Najla3 <chelly.n100@gmail.com>
2018-07-10 18:26:37 +0200

    ++CSs

---
+++ Ressources/Modals/portfolio_bloc.md.php
@@ -0,0 +1,108 @@
+<div class="col-<?php echo (empty($config["global"]["col"]))?"6":$config["global"]["col"];?>">
+    <div>
+        <section class="content-backoffice">
+            <div class="header-content">
+                <h4><?php echo (empty($config["global"]["title"]))?"No title in config array, please insert title":$config["global"]["title"];?></h4>
+                <?php if(!empty($config["global"]["icon_header"])):?>
+                    <?php foreach($config["global"]["icon_header"] as $key => $value): ?>
+                        <?php if($key == "modal"):?>
+                            <a href="#" class="target-modal active" data-type="open-modal" data-target="<?php echo $value["target"];?>"><i class="material-icons">&#xE145;</i></a>
+                        <?php elseif($key == "href"): ?>
+                            <a href="<?php echo $value["location"];?>" class="active"><i class="material-icons">&#xE145;</i></a>
+                        <?php else: ?>
+                            <a href="#" class="desactive"><i class="material-icons">&#xE145;</i></a>
+                        <?php endif; ?>
+                    <?php endforeach;?>
+                <?php endif;?>
+            </div>
+            <div class="main-content">
+                <table>
+                    <thead>
+                    <tr>
+                        <?php foreach($config["global"]["table_header"] as $value):?>
+                            <td><?php echo $value; ?></td>
+                        <?php endforeach;?>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php if(!empty($config["data"]["database"])):?>
+                        <?php foreach($config["data"]["array_data"] as $key => $arrayValue):?>
+                            <tr>
+                                <?php foreach($config["global"]["table_body_class"] as $className):?>
+                                    <?php if($className == "td-content-order"):?>
+                                        <?php if(isset($config["global"]["column_action_button"]["actionButtonOrder"])):?>
+                                            <td class="<?php echo $className;?>">
+                                                <a href="<?php echo $config["global"]["column_action_button"]["actionButtonOrder"].'/down/'.$arrayValue['data_id'];?>" class="btn-sm btn-tosle material-icons ordered-button">
+                                                    arrow_downward
+                                                </a>
+                                                <?php echo ($arrayValue["data_order"]) ?>
+                                                <a href="<?php echo $config["global"]["column_action_button"]["actionButtonOrder"].'/up/'.$arrayValue['data_id'];?>" class="btn-sm btn-tosle material-icons ordered-button">
+                                                    arrow_upward                                                </a>
+                                            </td>
+                                        <?php else: ?>
+                                            <td class="<?php echo $className;?>"><?php echo $key; ?></td>
+                                        <?php endif;?>
+                                    <?php endif;?>
+                                    <?php if($className == "td-content-text"):?>
+                                        <td class="<?php echo $className;?>"><?php echo $arrayValue["data_title"];?></td>
+                                    <?php endif;?>
+                                    <?php if($className == "td-content-date"):?>
+                                        <td class="<?php echo $className;?>"><?php echo $arrayValue["data_datecreate"];?></td>
+                                    <?php endif;?>
+                                    <?php if($className == "td-content-action"):?>
+                                        <td class="<?php echo $className;?>">
+                                            <?php foreach($config["global"]["column_action_button"] as $typeButton => $content):?>
+                                                <?php if($typeButton == "actionButtonEdit"):?>
+                                                    <?php if(isset($content)):?>
+                                                        <a href="<?php echo $config["data"]["data_href"]["edit"]."/".$arrayValue["data_id"];?>" class="btn-sm btn-orange"><?php echo $content;?></a>
+                                                    <?php endif;?>
+                                                <?php endif;?>
+                                                <?php if($typeButton == "actionButtonView"):?>
+                                                    <?php if(isset($content)):?>
+                                                        <a href="<?php echo $config["data"]["data_href"]["view"]."/".Access::constructUrl($arrayValue["data_title"]);?>" class="btn-sm btn-tosle"><?php echo $content;?></a>
+                                                    <?php endif;?>
+                                                <?php endif;?>
+                                                <?php if($typeButton == "actionButtonTarget"):?>
+                                                    <?php if(isset($content)):?>
+                                                        <a href="<?php echo $content["target"]."/".Access::constructUrl($arrayValue["data_title"]);?>" class="btn-sm btn-tosle"><?php echo $content['name'];?></a>
+                                                    <?php endif;?>
+                                                <?php endif;?>
+                                                <?php if($typeButton == "actionButtonStatus"):?>
+                                                    <?php if(isset($content)):?>
+                                                        <?php if($content[$arrayValue["data_status"]]["type"]=="href"):?>
+                                                            <a href="<?php echo $content[$arrayValue["data_status"]]["target"].$arrayValue["data_id"];?>" class="btn-sm btn-<?php echo $content[$arrayValue["data_status"]]["color"];?>"><?php echo $content[$arrayValue["data_status"]]["text"];?></a>
+                                                        <?php else:?>
+                                                            <button class="target-modal btn-sm btn-<?php echo $content[$arrayValue["data_status"]]["color"];?>" data-type="open-modal" data-target="<?php echo $content[$arrayValue["data_status"]]["target"];?>"><?php echo $content[$arrayValue["data_status"]]["text"];?></button>
+                                                        <?php endif;?>
+
+                                                    <?php endif;?>
+                                                <?php endif;?>
+                                            <?php endforeach;?>
+                                        </td>
+                                    <?php endif;?>
+                                <?php endforeach;?>
+                            </tr>
+                        <?php endforeach;?>
+                    <?php else:?>
+                        <?php foreach($config["data"]["array_data"] as $arrayValue):?>
+                            <tr>
+                                <?php foreach($arrayValue as $key => $value):?>
+                                    <?php if($key == "button_action"):?>
+                                        <?php if($value["type"]=="href"):?>
+                                            <td class="td-content-action"><a href="<?php echo $value["target"];?>" class="btn-sm btn-<?php echo $value["color"];?>"><?php echo $value["text"];?></a></td>
+                                        <?php else:?>
+                                            <td class="td-content-action"><button class="target-modal" data-type="open-modal" data-target="<?php echo $value["target"];?>"><?php echo $value["text"];?></button></td>
+                                        <?php endif;?>
+                                    <?php else: ?>
+                                        <td class="<?php echo $config["global"]["table_body_class"][$key];?>"><?php echo $value;?></td>
+                                    <?php endif;?>
+                                <?php endforeach;?>
+                            </tr>
+                        <?php endforeach;?>
+                    <?php endif;?>
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    </div>
+</div>
---
+++ Ressources/Modals/portfolio_form.md.php
@@ -0,0 +1,148 @@
+<?php if(!empty($errors)): ?>
+    <?php if(isset($errors['CODE_ERROR'])):?>
+        <section class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div>
+                        <h2>Une erreur est détectée sur votre fichier</h2>
+                        <p> Message d'erreur : <?php echo $errors['MESSAGE'];?></p>
+                        <p>Code d'erreur : <?php echo $errors['CODE_ERROR'];?></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <?php else:?>
+        <?php foreach($errors as $name => $value):?>
+            <?php echo $name. " ".$value;?>
+        <?php endforeach; ?>
+    <?php endif;?>
+<?php endif;?>
+<form method="<?php echo $config['config']['method'];?>" action="<?php echo $config['config']['action'];?>" <?php echo (isset($config['config']['form_file']) && $config['config']['form_file'])?"enctype='multipart/form-data'":"";?>>
+    <div class="form-group-base">
+        <?php if(isset($config['input'])):?>
+            <?php foreach ($config["input"] as $name => $attributs):?>
+                <?php if(isset($attributs["label"])):?>
+                    <label for="<?php echo $name;?>"><?php echo $attributs["label"];?></label>
+                <?php endif;?>
+                <?php if($attributs["type"]=="text" || $attributs["type"]=="email" || $attributs["type"]=="number" || $attributs["type"]=="password"):?>
+                    <input id="<?php echo $name;?>" type="<?php echo $attributs["type"];?>" placeholder="<?php echo $attributs["placeholder"];?>" name="<?php echo $name;?>" <?php echo (isset($attributs["required"]))?"required='required'":"";?> value="<?php echo (isset($config["data_content"][$name]))?$config["data_content"][$name]:"";?>">
+                <?php endif;?>
+                <?php if($attributs["type"]=="file"):?>
+                    <?php if(isset($config["data_content"]["file_img"])):?>
+                        <img src="<?php echo $config["data_content"]["file_img"];?>" width="200" height="100">
+                    <?php endif;?>
+                    <input id="<?php echo $name;?>" type="<?php echo $attributs["type"];?>" name="<?php echo $name;?>[]" <?php echo (isset($attributs["required"]) && $attributs["required"])?"required='required'":"";?> <?php echo (isset($attributs["multiple"]) && $attributs["multiple"])?"multiple":"";?>>
+                    <?php if(isset($config["data_content"]["file_path"])):?>
+                        <div class="small-precision-input">
+                            <a href="<?php echo $config["data_content"]["file_path"];?>" class="btn-sm btn-tosle">Télécharger le fichier</a>
+                        </div>
+                    <?php endif;?>
+                <?php endif;?>
+                <?php if(isset($attributs["description"])):?>
+                    <div class="small-precision-input">
+                        <?php echo $attributs["description"];?>
+                    </div>
+                <?php endif;?>
+            <?php endforeach;?>
+        <?php endif;?>
+        <?php if(isset($config["select_multiple"])): ?>
+            <?php foreach ($config["select_multiple"] as $select):?>
+                <?php foreach($select as $name => $attributs):?>
+                    <?php if($name == "category_select"):?>
+                        <?php if(isset($attributs["label"])):?>
+                            <label for="<?php echo $name;?>"><?php echo $attributs["label"];?></label>
+                        <?php endif;?>
+                        <select <?php echo (isset($attributs["multiple"]))?"multiple='multiple'":"";?> size="8" id="<?php echo $name;?>" name="<?php echo $name;?>[]">
+                            <?php foreach($attributs["options"] as $value => $text):?>
+                                <option value="<?php echo $value;?>" <?php echo (isset($config['data_content']['selectedOption'][$name][$value]))?"selected":"";?>><?php echo $text;?></option>
+                            <?php endforeach;?>
+                        </select>
+                        <?php if(isset($attributs["description"])):?>
+                            <div class="small-precision-input"><?php echo $attributs["description"];?></div>
+                        <?php endif;?>
+                    <?php endif;?>
+                    <?php if($name == "group_select"):?>
+                        <?php if(isset($attributs["label"])):?>
+                            <label for="<?php echo $name;?>"><?php echo $attributs["label"];?></label>
+                        <?php endif;?>
+                        <select <?php echo (isset($attributs["multiple"]))?"multiple='multiple'":"";?> size="8" id="<?php echo $name;?>" name="<?php echo $name;?>[]">
+                            <?php foreach($attributs["options"] as $value => $text):?>
+                                <option value="<?php echo $value;?>" <?php echo (isset($config['data_content']['selectedOption'][$value]))?"selected":"";?>><?php echo $text;?></option>
+                            <?php endforeach;?>
+                        </select>
+                        <?php if(isset($attributs["description"])):?>
+                            <div class="small-precision-input"><?php echo $attributs["description"];?></div>
+                        <?php endif;?>
+                    <?php endif;?>
+                    <?php if($name == "category_input"):?>
+                        <?php if(isset($attributs["label"])):?>
+                            <label for="<?php echo $name;?>"><?php echo $attributs["label"];?></label>
+                        <?php endif;?>
+                        <input id="<?php echo $name;?>" type="<?php echo $attributs["type"];?>" placeholder="<?php echo $attributs["placeholder"];?>" name="<?php echo $name;?>" <?php echo (isset($attributs["required"]) && $attributs["required"]==true )?"required='required'":"";?> value="<?php echo (isset($config["data_content"][$name]))?$config["data_content"][$name]:"";?>">
+                        <?php if(isset($attributs["description"])):?>
+                            <div class="small-precision-input"><?php echo $attributs["description"];?></div>
+                        <?php endif;?>
+                    <?php endif;?>
+                <?php endforeach;?>
+            <?php endforeach;?>
+        <?php endif; ?>
+        <?php if(isset($config["select"])): ?>
+            <?php foreach($config["select"] as $name => $attributs):?>
+                <?php if(isset($attributs["label"])):?>
+                    <label for="<?php echo $name;?>"><?php echo $attributs["label"];?></label>
+                <?php endif;?>
+                <select id="<?php echo $name;?>" name="<?php echo $name;?>" <?php echo (isset($attributs["required"]))?"required='required'":"";?>>
+                    <?php foreach($attributs["options"] as $value => $text):?>
+                        <option value="<?php echo $value;?>" <?php echo (isset($config['data_content'][$name]) && $config['data_content'][$name]==$value)?"selected":"";?>><?php echo $text;?></option>
+                    <?php endforeach;?>
+                </select>
+                <?php if(isset($attributs["description"])):?>
+                    <div class="small-precision-input"><?php echo $attributs["description"];?></div>
+                <?php endif;?>
+            <?php endforeach;?>
+        <?php endif; ?>
+        <?php if(isset($config["ckeditor"])):?>
+            <label for="<?php echo (isset($config["ckeditor"]["name"]))?$config["ckeditor"]["name"]:"No_name_found";?>"><?php echo ($attributs == "label")?$value:"Enter your article";?></label>
+        <?php if(isset($config["ckeditor"]["name"])):?>
+            <div id="alerts">
+                <noscript>
+                    <p>
+                        <strong>CKEditor requires JavaScript to run</strong>. In a browser with no JavaScript support, like yours, you should still see the contents (HTML data) and you should be able to edit it normally, without a rich editor interface.
+                    </p>
+                </noscript>
+            </div>
+            <textarea id="<?php echo $config["ckeditor"]["name"];?>" class="ckeditor" name ="<?php echo $config["ckeditor"]["name"];?>" placeholder="<?php echo (isset($config["ckeditor"]["placeholder"]))?$config["ckeditor"]["placeholder"]:"";?>" style="min-width: 100%; max-width: 100%; min-height: 700px; max-height: 1000px;" class="form-control">
+                    <?php if(isset($config["data_content"]["content"])):?>
+                        <?php echo $config["data_content"]["content"];?>
+                    <?php endif;?>
+                </textarea>
+            <script>var globalDirname = '<?php echo DIRNAME;?>Public/Libraries/ckeditor/';
+            </script>
+            <script type="text/javascript" src="<?php echo DIRNAME;?>Public/Libraries/ckeditor/ckeditor.js"></script>
+        <?php endif;?>
+        <?php if(isset($config["ckeditor"]["description"])):?>
+            <div class="small-precision-input"><?php echo $config["ckeditor"]["description"];?></div>
+        <?php endif;?>
+        <?php endif;?>
+        <?php if(isset($config["textarea"])):?>
+            <?php if(isset($config["textarea"]["label"])):?>
+                <label for="textArea_exampleBase"><?php echo $config["textarea"]["label"];?></label>
+            <?php endif;?>
+            <?php if(isset($config["textarea"]["name"])):?>
+                <textarea id="<?php echo $config["textarea"]["name"];?>" name="<?php echo $config["textarea"]["name"];?>" class="textarea-col-12" placeholder="<?php echo (isset($config["textarea"]["placeholder"])?$config["textarea"]["placeholder"]:"");?>"><?php if(isset($config["data_content"]["content"])):?><?php echo $config["data_content"]["content"];?><?php endif;?></textarea>
+            <?php endif;?>
+            <?php if(isset($config["textarea"]["description"])):?>
+                <div class="small-precision-input"><?php echo $config["textarea"]["description"];?></div>
+            <?php endif;?>
+        <?php endif;?>
+        <div>
+            Voici les différentes options du formulaire
+        </div>
+        <?php if(isset($config["exit"])):?>
+            <a href="<?php echo $config["exit"];?>" class="btn btn-red">Exit</a>
+        <?php endif;?>
+        <?php if(isset($config["config"]["save"])):?>
+            <button type="submit" class="btn btn-orange" name="save-draft"><?php echo $config["config"]["save"];?></button>
+        <?php endif;?>
+        <button type="submit" class="btn btn-green" name="publish"><?php echo $config["config"]["submit"];?></button>
+    </div>
