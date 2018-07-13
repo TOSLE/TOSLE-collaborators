b3afdb5df50fead67d9b8ec12ffd41d5465b7f3a
Backins <backinsmail@gmail.com>
2018-07-13 20:40:04 +0200

    Update nom fonction CoreSql

---
+++ Kernel/Class/Analytics.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: julien
+ * Date: 13/07/2018
+ * Time: 20:02
+ */
+
+class Analytics
+{
+
+}
--- Src/Models/Stats.class.php
+++ Src/Models/Stats.class.php
@@ -6,15 +6,37 @@
  * Time: 23:52
  */
 
-class Stat extends CoreSql {
+/**
+ * Class Statsview
+ * id => id unique de la table
+ * type => type de données
+ * token => token identique au cookie
+ * day => jour
+ * month => mois
+ * year => année
+ * target => cible
+ * source => source
+ * Le type, le target et la source sont un ensemble. On pourrait imaginer :
+ *      - vue : target(view), source(referer), cible(page)
+ *      - upload : target(upload), source(utilisateur), cible(type fichier)
+ *          * source, id de l'utilisateur
+ *          * target, on peut imaginer avatar/devoirs/etc.
+ *      - etc
+ */
+class Stats extends CoreSql {
 
     protected $id;
-    protected $name;
-    protected $value;
+    protected $type;
+    protected $token;
+    protected $day;
+    protected $month;
+    protected $year;
+    protected $target;
+    protected $source;
 
     public function __construct()
     {
-        //parent::__construct();
+        parent::__construct();
     }
 
     /**
