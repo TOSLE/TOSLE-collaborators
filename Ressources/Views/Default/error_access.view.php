1594ba204e41bd6b19cf4410135421e0c493dcaf
Samy Immoula <immoulasamy@gmail.com>
2018-07-24 18:25:26 +0200

    Page 403

---
+++ Ressources/Views/Default/error_access.view.php
@@ -0,0 +1,26 @@
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div>
+                <div class="row">
+                    <div class="col-10">
+                        <div>
+                            <section class="container-message">
+                                <section class="message">
+                                    <i class="material-icons">
+                                        remove_circle
+                                    </i>
+                                    <h2>Error 403</h2>
+                                    <p>You don't have the permission to access </p>
+                                    <div class="buttondiv">
+                                        <a class="btn btn-blue-outline" href="<?php echo DIRNAME ;?>">Back  to the homepage</a>
+                                        <a class="btn btn-green-outline" href="<?php echo DIRNAME ;?>">Sign in</a>
+                                    </div>
+                                </section>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ Ressources/Views/error_access.view.php
@@ -0,0 +1,24 @@
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div>
+                <div class="row">
+                    <div class="col-10">
+                        <div>
+                            <section class="container-message">
+                                <section class="message">
+                                    <i class="material-icons">
+                                        remove_circle
+                                    </i>
+                                    <h2>403</h2>
+                                    <p>That's an error.</p>
+                                    <p>The requested URL <b><?php echo $_SERVER['REQUEST_URI']; ?></b> was not found.</p>
+                                    <a href="<?php echo DIRNAME ?>">Click here to be redirected to the homepage.</a>
+                                </section>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
